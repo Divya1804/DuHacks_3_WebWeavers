654b91a7e74db8c9ff7ac2b45d7e703933b14f23
Rudra112004 <rudrapatel112004@gmail.com>
2024-02-24 13:07:58 +0530

    Event entity created

---
+++ server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/entity/Event.java
@@ -0,0 +1,52 @@
+package com.seva.sangam.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eventId;
+
+    @Column(name = "event_name")
+    private String eventName;
+
+    @Column(name="event_description", length = 10000)
+    private String details;
+
+    @Column(name="event_photo", length = 2048)
+    private String photo;
+
+    @Column(name = "event_type")
+    private String type;
+
+    @Column(name = "event_location")
+    private String location;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name="gained_amount")
+    private Long gainedAmount;
+
+    @Column(name="required_amount")
+    private Long requiredAmount;
+
+
+}
+
