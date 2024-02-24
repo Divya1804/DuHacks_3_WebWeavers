8fc94dfd7e3ff994a7a1c85789dc97bf9daa87af
Rudra112004 <rudrapatel112004@gmail.com>
2024-02-24 16:47:39 +0530

    event controller created and (create event api created)

---
+++ server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/controller/EventController.java
@@ -0,0 +1,23 @@
+package com.seva.sangam.controller;
+
+import com.seva.sangam.payload.EventDto;
+import com.seva.sangam.service.EventServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/")
+@CrossOrigin
+public class EventController {
+
+    @Autowired
+    private EventServices eventServices;
+
+    @PostMapping("/ngo/{ngoId}/event")
+    private ResponseEntity<?> createEvent(@RequestBody EventDto eventDto, @PathVariable("ngoId") Long ngoId){
+        EventDto event = eventServices.createEvent(eventDto, ngoId);
+        return new ResponseEntity<EventDto>(event, HttpStatus.CREATED);
+    }
+}
