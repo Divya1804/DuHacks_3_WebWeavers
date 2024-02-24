fd05a5e023496684b4ddffb067510b5dec5de1ba
brijpatel-2003 <brijpatidar2003@gmail.com>
2024-02-24 14:39:23 +0530

    Added Footer and Navbar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+let apiKey =    import.meta.env.VITE_API_KEY;
+let backendUrl =import.meta.env.VITE_BACKEND_URL;
+
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(apiKey);
   return (
     <>
        <h1>hello</h1>
