ffb85387075f40390280ab3a12d320ebbf35d0f0
Rudra112004 <rudrapatel112004@gmail.com>
2024-02-24 16:46:16 +0530

    event controller created and (create event api created)

--- server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/service/EventServices.java
+++ server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/service/EventServices.java
@@ -1,7 +1,9 @@
 package com.seva.sangam.service;
 
+import com.seva.sangam.payload.EventDto;
+
 import java.util.List;
 
 public interface EventServices {
-
+    EventDto createEvent(EventDto eventDto, Long ngoId);
 }
--- server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/service/impl/EventServiceImpl.java
+++ server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/service/impl/EventServiceImpl.java
@@ -1,12 +1,39 @@
 package com.seva.sangam.service.impl;
 
 
+import com.seva.sangam.entity.Event;
+import com.seva.sangam.entity.NgoAdmin;
+import com.seva.sangam.exception.ResourceNotFound;
+import com.seva.sangam.payload.EventDto;
+import com.seva.sangam.repository.EventRepo;
+import com.seva.sangam.repository.NgoRepo;
 import com.seva.sangam.service.EventServices;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 
 @Service
 public class EventServiceImpl implements EventServices {
+
+    @Autowired
+    private EventRepo eventRepo;
+
+    @Autowired
+    private NgoRepo ngoRepo;
+
+    @Autowired
+    private ModelMapper model;
+    @Override
+    public EventDto createEvent(EventDto e, Long ngoId) {
+
+        Event event = model.map(e, Event.class);
+        NgoAdmin ngoAdmin = ngoRepo.findById(ngoId).orElseThrow(() -> new ResourceNotFound("Ngo", "Id", ngoId));
+        event.setNgoAdmin(ngoAdmin);
+        Event event1 = eventRepo.save(event);
+
+        return model.map(event1, EventDto.class);
+    }
 }
 
