cb04603f1de3ed3c0a8010a094b37a427aca68c4
danishppatel <danishpatel5113@gmail.com>
2024-02-24 16:27:39 +0530

    I have Created NGOsList.jsx and EventList.jsx components

---
+++ client/src/components/NGOsList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NGOsList({ result }) {
+  return (
+    <>
+      <div className="shadow-md ">
+        <h3 className="text-lg font-bold mb-2 mt-6 ml-4">{result.length} NGOs found</h3>
+        <div className="m-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+          {result}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NGOsList;
