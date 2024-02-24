e392e4fd3af5776a0df877d1ee2016198a7626cc
Divya1804 <pateldivya1804@gmail.com>
2024-02-24 13:09:04 +0530

    NgoAdmin entity Created

---
+++ server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/entity/NgoAdmin.java
@@ -0,0 +1,55 @@
+package com.seva.sangam.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class NgoAdmin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ngoId;
+
+    @Column(name = "ngo_name")
+    private String ngoName;
+
+    @Column(name = "ngo_slogan")
+    private String slogan;
+
+    @Column(name = "ngo_location")
+    private String location;
+
+    @Column(name = "ngo_certi", length = 2000)
+    private String certiLink;
+
+    @Column(name = "ngo_logo", length = 2000)
+    private String logoLink;
+
+    @Column(name = "ngo_about", length = 4000)
+    private String about;
+
+    @Column(name = "ngo_username", unique = true, nullable = false)
+    private String ngoUserName;
+
+    @Column(name = "ngo_password", nullable = false)
+    private String ngoPassword;
+
+    @Column(name = "ngo_email")
+    private String ngoEmail;
+
+    @Column(name = "ngo_phoneno")
+    private Long ngoPhoneNo;
+
+    private String merchantId;
+    private String secretKey;
+    private String payEmail;
+    private Long payPhoneNo;
+
+}
