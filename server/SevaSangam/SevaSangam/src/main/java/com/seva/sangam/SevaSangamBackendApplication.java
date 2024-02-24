a16cb5a3acdce3797ef52ad4ae57e7211f0c61ee
Divya1804 <pateldivya1804@gmail.com>
2024-02-24 12:26:46 +0530

    server added

---
+++ server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/SevaSangamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seva.sangam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevaSangamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevaSangamBackendApplication.class, args);
+	}
+
+}
---
+++ server/SevaSangam/SevaSangam/src/test/java/com/seva/sangam/SevaSangamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seva.sangam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevaSangamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
