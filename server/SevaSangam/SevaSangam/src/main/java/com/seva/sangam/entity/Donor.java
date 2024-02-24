2e247fcb5c28e687f683b1694f85c418fa209bd9
Divya1804 <pateldivya1804@gmail.com>
2024-02-24 13:03:37 +0530

    Donor entity Created

---
+++ server/SevaSangam/SevaSangam/src/main/java/com/seva/sangam/entity/Donor.java
@@ -0,0 +1,48 @@
+package com.seva.sangam.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Donor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long donorId;
+
+    @Column(name = "donor_name")
+    private String donorName;
+
+    @Column(name = "user_name",  unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "user_password",  nullable = false)
+    private String password;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "phone_no")
+    private Long phoneNo;
+
+    @Column(name = "occupation")
+    private String occupation;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "paid_amount")
+    private Long amount;
+
+    @Column(name = "photo_link")
+    private String photoLink;
+
+
+}
